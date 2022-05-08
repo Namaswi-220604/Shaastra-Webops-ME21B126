d0f17b36a84177c0fa9ef3b010fb324ec8533d4d
Namaswi <namaswi1729@gmail.com>
2022-05-08 06:54:10 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import TodoList from './essentials/TodoList';
+
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+      
+    </div>
+  );
+}
+
+export default App;
